b6f421b8a3fc65d0e4707e692728b1b318ade3bf
Joseph Mearman <joseph@mearman.co.uk>
2026-04-24 15:50:17 +0100

    chore: add commit hooks and commit linting

---
+++ commitlint.config.ts
@@ -0,0 +1,25 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
+    "subject-full-stop": [2, "never", "."],
+    "body-max-line-length": [1, "always", 200],
+  },
+};
---
+++ lint-staged.config.ts
@@ -0,0 +1,5 @@
+export default {
+  "*.{ts,tsx}": ["eslint --fix --cache"],
+  "*.json": ["eslint --fix --cache"],
+  "*.md": ["eslint --fix --cache"],
+};
